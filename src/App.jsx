310326afeaa05b256e016f83322927f9f3a660c6
damidevel0per <damidevtech@gmail.com>
2025-08-31 18:38:41 -0300

    subiendo section de servicios brindados

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Nosotros from './components/Nosotros'
+import Servicios from './components/Servicios'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Nosotros/>
+      <Servicios/>
     </>
   )
 }
