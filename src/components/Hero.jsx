0591a93d90db29a5d2938abaeac39d05be978b67
damidevel0per <damidevtech@gmail.com>
2025-08-31 17:19:19 -0300

    agregando section sobre nosotros con estilos e imagenes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Bunny from '../assets/BUNNY-HERO.PNG'
 const Hero = () => {
   return (
-    <section className='bg-white-50 flex justify-around items-center py-20 h-[80vh] sm:px-12 sm:py-6 px-4 py-3'>
+    <section className='bg-white-50 flex justify-around items-center py-20 h-[90vh] sm:px-12 sm:py-6 px-4 py-3'>
         <div>
             <h1 className='text-5xl text-yellow'>Una mascota saludable <br />es una mascota <span className='text-yellow-300'>feliz!</span></h1>
             <div className='py-5'>
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import Chinchilla from '../assets/CHINCHILLA.PNG'
+import Bunny from '../assets/BUNNY.png'
+import PerroGato from '../assets/PERROYGATO.png'
 
 const Servicios = () => {
   return (
-    <section className='bg-yellow-300 h-[90vh]'>
+    <section className=' bg-yellow-300'>
         <div>
+          <h2 className='py-15 flex justify-center text-white text-5xl'>Sobre Nosotros</h2>
         </div>
+
+        <div>
+        <div className='bg-white-50 flex justify-around items-center py-20 sm:px-12 sm:py-6 px-4 py-3'>
+          <div>
+              <img className='w-[400px]' src={Chinchilla} alt="Chinchilla" />
+          </div>
+          <div className='text-right'>
+            <p className='text-3xl'>En <span className='text-yellow-600'>ExoVet</span> nos dedicamos a brindar atención especializada a 
+            <br /> mascotas exóticas y no convencionales.</p>
+            <p className='text-3xl'>Nuestra filosofía se basa en el <span className='text-yellow-600'>respeto, cuidado y bienestar de cada animal</span>, <br /> 
+              ofreciendo tratamientos personalizados según sus necesidades.</p>
+          </div>
+        </div>
+
+        <div className='bg-white-50 flex justify-around items-center py-20 sm:px-12 sm:py-6 px-4 py-3'>
+          <div className='text-left'>
+            <p className='text-3xl'>Contamos con un equipo profesional y comprometido. <br /> con experiencia en 
+            <span className='text-yellow-600'> reptiles, aves, roedores y otros animales no convencionales</span> <br />
+            garantizando que cada visita sea segura y cómoda para tu mascota.</p>
+          </div>
+          <div>
+              <img className='w-[400px]' src={Bunny} alt="Conejo blanco y negro" />
+          </div>
+        </div>
+
+        <div className='py-20 sm:px-12 sm:py-6 px-10 py-3'>
+          <p className='text-3xl text-center'>Además de nuestra especialización en animales exóticos, en ExoVet también brindamos atención integral <br /> a mascotas convencionales <span className='text-yellow-600'>como perros y gatos</span>, garantizando el mismo compromiso, cuidado y <br /> profesionalismo para <span className='text-yellow-600'>todos los miembros de tu familia.</span></p>
+        </div>
+        <div className='flex justify-center'>
+          <img className='w-[500px]' src={PerroGato} alt="Conejo blanco y negro" />
+        </div>
+        </div>
+
     </section>
 
   )
