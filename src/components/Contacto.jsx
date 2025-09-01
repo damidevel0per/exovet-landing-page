defa08fd87a18c8c658baa604e3d74b5b62cee3d
damidevel0per <damidevtech@gmail.com>
2025-09-01 15:34:59 -0300

    editando section de contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -6,7 +6,7 @@ const Contacto = () => {
         <section id="contacto" className='py-15 bg-[url("data:image/svg+xml,%3Csvg%20width%3D%2720%27%20height%3D%2720%27%20viewBox%3D%270%200%2020%2020%27%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cg%20fill%3D%22%238200db%22%20fill-opacity%3D%220.03%22%20fill-rule%3D%22evenodd%22%3E%3Ccircle%20cx%3D%223%22%20cy%3D%223%22%20r%3D%223%22/%3E%3Ccircle%20cx%3D%2213%22%20cy%3D%2213%22%20r%3D%223%22/%3E%3C/g%3E%3C/svg%3E")] w-full bg-[length:100px_105px]'>
 
             <div className=''>
-                <h2 className='font-semibold py-15 text-center text-black text-5xl'>Contactate con nosotros</h2>
+                <h2 className='font-semibold py-15 text-center text-purple-700 text-5xl'>Contactate con nosotros</h2>
             </div>
             
             <div className='flex justify-center'>
@@ -14,20 +14,20 @@ const Contacto = () => {
                     className='shadow-xl flex flex-col w-full max-w-2xl mx-5 md:mx-auto rounded-2xl text-purple-900 bg-yellow-300 font-semibold p-8' 
                     action=""
                 >
-                    <h2 className='text-3xl text-center mb-6 text-gray-900'>Completa los campos</h2>
+                    <h2 className='text-2xl text-center mb-6 text-purple-700'>Completa los campos</h2>
 
                     <div className='flex flex-col mb-4'>
-                        <label className='text-2xl mb-2 text-gray-900' htmlFor="">Nombre</label>
+                        <label className='text-xl mb-2 text-purple-700' htmlFor="">Nombre</label>
                         <input className='inset-shadow-xs bg-white rounded my-2 px-3 h-10 rounded w-full' placeholder='Ej: Damian Perez' type="text" />
                     </div>
 
                     <div className='flex flex-col mb-4'>
-                        <label className='text-2xl mb-2 text-gray-900' htmlFor="">Email</label>
+                        <label className='text-xl mb-2 text-purple-700' htmlFor="">Email</label>
                         <input className='inset-shadow-xs bg-white rounded my-2 px-3 h-10 rounded w-full' placeholder='correo@email.com' type="email" />
                     </div>
 
                     <div className='flex flex-col mb-4'>
-                        <label className='text-2xl mb-2 text-gray-900' htmlFor="">Mensaje</label>
+                        <label className='text-xl mb-2 text-purple-700' htmlFor="">Mensaje</label>
                         <textarea className='inset-shadow-xs bg-white rounded my-2 px-3 py-3 h-40 rounded w-full' placeholder='Dejanos un mensaje!' type="text" />
                     </div>
 
@@ -42,7 +42,7 @@ const Contacto = () => {
             </div>
             
             <div className='flex flex-col items-center justify-center my-8'>
-                <h2 className='font-semibold py-25 text-center text-black text-5xl'>También puedes venir a visitarnos!</h2>
+                <h2 className='font-semibold py-25 text-center text-purple-700 text-5xl'>También podés venir a visitarnos!</h2>
                 <MapaDireccion/>
             </div>
         </section>
