bf0002721c98e28ec5c8e0feff222461c8ca5814
damidevel0per <damidevtech@gmail.com>
2025-09-01 08:37:39 -0300

    agregando patrones svg en fondo de distintas sections y cambiando colores

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -6,12 +6,12 @@ import PerroGato from '../assets/PERROYGATO.png'
 const Nosotros = () => {
   return (
     <section className=''>
-        <div>
-          <h2 className='font-semibold py-15 flex justify-center text-black text-5xl'>Sobre nosotros</h2>
+        <div className='bg-purple-700'>
+          <h2 className='font-semibold py-15 flex justify-center text-white text-5xl'>Sobre nosotros</h2>
         </div>
 
         <div>
-        <div className='h-[100vh] bg-yellow-300 flex justify-around items-center py-20 sm:px-12 sm:py-6 px-4 py-3'>
+        <div className='h-[101vh] bg-yellow-300 flex justify-around items-center py-20 sm:px-12 sm:py-6 px-4 py-3'>
           <div>
               <img className='w-[400px]' src={Chinchilla} alt="Chinchilla" />
           </div>
@@ -23,7 +23,8 @@ const Nosotros = () => {
           </div>
         </div>
 
-        <div className='h-[100vh] bg-white flex justify-around items-center py-20 sm:px-12 sm:py-6 px-4 py-3'>
+        <div className='bg-[url("data:image/svg+xml,%3Csvg%20width%3D%27100%27%20height%3D%2720%27%20viewBox%3D%270%200%20100%2020%27%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cpath%20d%3D%27M21.184%2020c.357-.13.72-.264%201.088-.402l1.768-.661C33.64%2015.347%2039.647%2014%2050%2014c10.271%200%2015.362%201.222%2024.629%204.928.955.383%201.869.74%202.75%201.072h6.225c-2.51-.73-5.139-1.691-8.233-2.928C65.888%2013.278%2060.562%2012%2050%2012c-10.626%200-16.855%201.397-26.66%205.063l-1.767.662c-2.475.923-4.66%201.674-6.724%202.275h6.335zm0-20C13.258%202.892%208.077%204%200%204V2c5.744%200%209.951-.574%2014.85-2h6.334zM77.38%200C85.239%202.966%2090.502%204%20100%204V2c-6.842%200-11.386-.542-16.396-2h-6.225zM0%2014c8.44%200%2013.718-1.21%2022.272-4.402l1.768-.661C33.64%205.347%2039.647%204%2050%204c10.271%200%2015.362%201.222%2024.629%204.928C84.112%2012.722%2089.438%2014%20100%2014v-2c-10.271%200-15.362-1.222-24.629-4.928C65.888%203.278%2060.562%202%2050%202%2039.374%202%2033.145%203.397%2023.34%207.063l-1.767.662C13.223%2010.84%208.163%2012%200%2012v2z%27%20fill%3D%22%23ffdf20%22%20fill-opacity%3D%220.099%22%20fill-rule%3D%22evenodd%22/%3E%3C/svg%3E")] 
+                          bg-[length:250px_50px] h-[101vh] bg-white flex justify-around items-center py-20 sm:px-12 sm:py-6 px-4 py-3'>
           <div className='text-left'>
             <p className='text-3xl'>Contamos con un equipo profesional y comprometido. <br /> con experiencia en 
             <span className='text-purple-700'> reptiles, aves, roedores y otros animales no convencionales</span> <br />
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -6,37 +6,37 @@ import Estudio from '../assets/ESTUDIO-CLINICO.JPG'
 
 const Servicios = () => {
   return (
-    <section className='py-20'>
+    <section className='py-20 bg-purple-700'>
     <div>
         <div>
-          <h2 className='font-semibold py-15 flex justify-center text-black text-5xl'>Servicios que brindamos</h2>
+          <h2 className='font-semibold py-15 flex justify-center text-white text-5xl'>Servicios que brindamos</h2>
         </div>
     </div>
 
-    <div class="grid grid-cols-2 divide-x-4 divide-purple-700">
+    <div class="grid grid-cols-2 divide-x-4 divide-yellow-300">
         <div className='flex flex-col items-center'>
-            <h2 className='font-semibold text-2xl text-center'>Atención en nuestro consultorio</h2>
-            <p className='text-lg text-center mb-6'>Veni con o sin cita previa a plantearnos tu duda sobre tu mascota.</p>
-            <img className='w-[600px] max-w-full rounded-lg shadow-lg' src={BunnyConsulta} alt="" />
+            <h2 className='font-semibold text-2xl text-center text-yellow-300'>Atención en nuestro consultorio</h2>
+            <p className='text-lg text-center mb-6 text-yellow-100'>Veni con o sin cita previa a plantearnos tu duda sobre tu mascota.</p>
+            <img className='grayscale w-[600px] max-w-full rounded-lg shadow-lg hover:scale-102 hover:grayscale-0 transition duration-500 ' src={BunnyConsulta} alt="" />
         </div>
         <div className='flex flex-col items-center'>
-            <h2 className='font-semibold text-2xl text-center'>Clinica y cirugia</h2>
-            <p className='text-lg text-center mb-6'>Cirugia y esterilizaciones de felinos, caninos y exóticos.</p>
-            <img className='w-[600px] max-w-full rounded-lg shadow-lg' src={PerroCirugia} alt="" />
+            <h2 className='font-semibold text-2xl text-center text-yellow-300'>Clinica y cirugia</h2>
+            <p className='text-lg text-center mb-6 text-yellow-100'>Cirugia y esterilizaciones de felinos, caninos y exóticos.</p>
+            <img className='grayscale w-[600px] max-w-full rounded-lg shadow-lg hover:scale-102 hover:grayscale-0 transition duration-500' src={PerroCirugia} alt="" />
         </div>
     </div>
 
     <div class="py-20 grid grid-cols-2 divide-x-4 divide-purple-700">
         <div className='flex flex-col items-center'>
-            <h2 className='font-semibold text-2xl text-center'>Diagnóstico y estudios</h2>
-            <p className='text-lg text-center mb-6'>Análisis, radiografías y estudios complementarios.</p>
-            <img className='w-[600px] max-w-full rounded-lg shadow-lg' src={Estudio} alt="" />
+            <h2 className='font-semibold text-2xl text-center text-yellow-300'>Diagnóstico y estudios</h2>
+            <p className='text-lg text-center mb-6 text-yellow-100'>Análisis, radiografías y estudios complementarios.</p>
+            <img className='grayscale w-[600px] max-w-full rounded-lg shadow-lg hover:scale-102 hover:grayscale-0 transition duration-500 ' src={Estudio} alt="" />
         </div>
 
         <div className='flex flex-col items-center'>
-            <h2 className='font-semibold text-2xl text-center'>Atención a domicilio</h2>
-            <p className='text-lg text-center mb-6'>Vamos a donde tu mascota nos necesite.</p>
-            <img className='w-[600px] max-w-full rounded-lg shadow-lg' src={PerroDomicilio} alt="" />
+            <h2 className='font-semibold text-2xl text-center text-yellow-300'>Atención a domicilio</h2>
+            <p className='text-lg text-center mb-6 text-yellow-100'>Vamos a donde tu mascota nos necesite.</p>
+            <img className='grayscale w-[600px] max-w-full rounded-lg shadow-lg hover:scale-102 hover:grayscale-0 transition duration-500 ' src={PerroDomicilio} alt="" />
         </div>
     </div>
 
